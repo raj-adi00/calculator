5433b2c46f104251b28d8567ec3608d2fd2f823c
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-02-14 13:34:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let button;
+let symbol;
+const tokens = ["+", "-", "*", "/", 1, 2, 3, "=", 4, 5, 6, 7, 8, 9, 0, ".", "AC", "DEL"];
+const target = document.getElementById("buttons");
+for (let i = 0; i < tokens.length; i++) {
+  button = document.createElement("button");
+  symbol = document.createTextNode(tokens[i]);
+  button.appendChild(symbol);
+  button.setAttribute("class", "btn");
+  target.appendChild(button);
+  if (i == 7) {
+    button.setAttribute("id", "equal");
+    button.removeAttribute("class");
+  }
+  button.classList.add("font");
+}
+let clicked_button = document.querySelectorAll(".btn");
+
+for (let index = 0; index <= 14; index++) {
+  clicked_button[index].addEventListener("click", () => {
+    document.getElementById("screen").innerText += `${clicked_button[index].innerText}`;
+  })
+}
+clicked_button[16].addEventListener("click", () => {
+  document.getElementById("screen").innerText = document.getElementById("screen").innerText.slice(0, document.getElementById("screen").innerText.length - 1);
+});
+clicked_button[15].addEventListener("click",()=>{
+  document.getElementById("screen").innerText="";
+});
+document.getElementById("equal").addEventListener("click",()=>{
+   let operation=document.getElementById("screen").innerText;
+   document.getElementById("screen").innerText=eval(operation);
+});
+
